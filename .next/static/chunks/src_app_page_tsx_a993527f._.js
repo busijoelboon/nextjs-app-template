c1d9df065b7fa2a876d5ef8bedf4e866f8a6c859
busijoelboon <joelboon10@gmail.com>
2025-07-17 20:59:34 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1752766173904

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_824e559c._.js",
+  "static/chunks/src_e5381081._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_jspdf_dist_jspdf_es_min_c277e70f.js",
+  "static/chunks/node_modules_html2canvas_dist_html2canvas_3938cdad.js",
+  "static/chunks/node_modules_4c3cad60._.js"
+],
+    source: "dynamic"
+});
